3617fd5e916c68068d3c91452697a807bbc6cdfb
muthenna <muthennamaru@gmail.com>
2016-07-27 16:09:39 -0500

    Added a comment in AFConstants class

--- app/src/main/java/com/example/akshi/afpomotion/AFConstants.java
+++ app/src/main/java/com/example/akshi/afpomotion/AFConstants.java
@@ -5,6 +5,8 @@ package com.example.akshi.afpomotion;
  */
 public class AFConstants {
 
+    //these are the constants for this application
+
     public static final String AF_URL = "https://www.abercrombie.com/anf/nativeapp/Feeds/promotions.json";
     public static final String ON_LINE = "on_line";
     public static final String TARGET_URL = "target_url";
